e369685a5e1c0c39023ec1c89ded47fa8d1f3efd
HouseoLogy <thehouseology@gmail.com>
2025-06-16 15:24:02 +0200

    add stack description and tag

--- app.py
+++ app.py
@@ -6,16 +6,35 @@ from comfyui_aws_stack.comfyui_aws_stack import ComfyUIStack
 from cdk_nag import NagSuppressions, AwsSolutionsChecks
 
 app = cdk.App()
-comfy_ui_stack = ComfyUIStack(app, 
-                              "ComfyUIStack",
-                              env=Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"],
-                                              region=os.environ["CDK_DEFAULT_REGION"]))
+comfy_ui_stack = ComfyUIStack(
+    app,
+    "ComfyUIStack",
+    description="End2end ComfyUI Stack on AWS (uksb-be9535xn3q)",
+    env=Environment(
+        account=os.environ["CDK_DEFAULT_ACCOUNT"],
+        region=os.environ["CDK_DEFAULT_REGION"],
+    ),
+    tags={"Repository": "aws-samples/comfyui-personalized-generative-ai-avatars-app"},
+)
+
 
 Aspects.of(app).add(AwsSolutionsChecks(verbose=False))
-NagSuppressions.add_stack_suppressions(stack=comfy_ui_stack, suppressions=[
-    { "id": "AwsSolutions-IAM4", "reason": "For sample purposes the managed policies are good enough" },
-    { "id": "AwsSolutions-IAM5", "reason": "Some rules require '*' wildcard as an example ACM operations, and others are sufficient for Sample" },
-    { "id": "AwsSolutions-ECS2", "reason": "The cdk stack provides only non-sensitive data as environment variables. The sensitive ones are alreay provided as secrets" }
-])
+NagSuppressions.add_stack_suppressions(
+    stack=comfy_ui_stack,
+    suppressions=[
+        {
+            "id": "AwsSolutions-IAM4",
+            "reason": "For sample purposes the managed policies are good enough",
+        },
+        {
+            "id": "AwsSolutions-IAM5",
+            "reason": "Some rules require '*' wildcard as an example ACM operations, and others are sufficient for Sample",
+        },
+        {
+            "id": "AwsSolutions-ECS2",
+            "reason": "The cdk stack provides only non-sensitive data as environment variables. The sensitive ones are alreay provided as secrets",
+        },
+    ],
+)
 
 app.synth()
